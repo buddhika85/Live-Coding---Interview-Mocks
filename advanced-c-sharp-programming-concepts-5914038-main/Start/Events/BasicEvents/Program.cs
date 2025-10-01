ac50693ab6bcb1e0a76eff74939707b469d59bda
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-10-01 13:42:39 +1000

    Adv C# course files added

---
+++ advanced-c-sharp-programming-concepts-5914038-main/Start/Events/BasicEvents/Program.cs
@@ -0,0 +1,47 @@
+﻿// LinkedIn Learning Course exercise file for Advanced C# Programmin by Joe Marini
+// Example file for basic events
+
+namespace BasicEvents
+{
+    // define the delegate for the event handler
+    public delegate void MyEventHandler(string value);
+
+    class EventPublisher {
+        private string TheVal = "";
+
+        // TODO: declare the event
+
+        public string Val {
+            set {
+                this.TheVal = value;
+                // TODO: when the value changes, fire the event
+
+            }
+        }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+            EventPublisher obj = new EventPublisher();
+            // TODO: use a named function as an event handler
+
+            // TODO: use an anonymous delegate as an event handler
+
+
+            string? str = "";
+            do {
+                Console.WriteLine("Enter a value: ");
+                str = Console.ReadLine();
+                if (!str!.Equals("exit")) {
+                    obj.Val = str;
+                }
+            } while (!str.Equals("exit"));
+            Console.WriteLine("Goodbye!");
+        }
+
+        // This function will be called when the value changes in the EventPublisher class
+        static void ObjValueChanged(string value) {
+            Console.WriteLine("The value changed to {0}", value);
+        }
+    }
+}
---
+++ advanced-c-sharp-programming-concepts-5914038-main/Start/Lambdas/BasicLambdas/Program.cs
@@ -0,0 +1,22 @@
+﻿// LinkedIn Learning Course exercise file for Advanced C# Programming by Joe Marini
+// Example file for basic lambda functions
+
+namespace BasicLambdas
+{
+    // define a few delegate types
+    public delegate int MyDelegate(int x);
+    public delegate void MyDelegate2(int x, string prefix);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a basic delegate that squares a number
+
+            // Dynamically change the delegate to something else
+
+            // Create a delegate that takes multiple arguments
+
+        }
+    }
+}
