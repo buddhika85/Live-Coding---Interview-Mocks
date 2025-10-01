acb08a7ad1b298042214030486104aac84e9a911
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-10-01 14:12:31 +1000

    learning

--- advanced-c-sharp-programming-concepts-5914038-main/Start/Language/IndexesRanges/Program.cs
+++ advanced-c-sharp-programming-concepts-5914038-main/Start/Language/IndexesRanges/Program.cs
@@ -1,21 +1,37 @@
-﻿// LinkedIn Learning Course exercise file for Advanced C# Programming by Joe Marini
-// Indexes and Ranges example
+﻿using static System.Console;
 
 // Define a sample array 
 string[] words = [
-    "The","quick","brown","fox","jumps",
-    "over","the","lazy","dog"       
+    "First", "Second", "Third", "Forth", "Fifth",
+    "Sixth", "Seventh", "Eighth", "Nineth"      
 ];
 
-// The index operator provides access to array elements
-Console.WriteLine(words[1]);
 
-// The index-from-end operator indexes from the end of a sequence
-// (Note that the end is not zero-based, ^1 is the last item)
+          
+
+// The index-from-end operator or hat operator (^) indexes from the end of a sequence
+WriteLine(words[^1]);                   // give me last index       , same as WriteLine(words[words.Length - 1]);
+WriteLine(words[^2]);                   // give me second from last index
+
 
 
 // The range operator (..) defines a range
+string[] wordRange = words[2..5];           // give me index 2 to 5
+WriteLine($"{string.Join(",", wordRange)}");
 
+wordRange = words[..];                      // give me a copy of words
+WriteLine($"{string.Join(",", wordRange)}");
+
+wordRange = words[2..];                         // give me all after 2
+WriteLine($"{string.Join(",", wordRange)}");
+
+wordRange = words[..5];                         // give me all before 5
+WriteLine($"{string.Join(",", wordRange)}");
 
 // Indexes and ranges can be variables too
+Index idx = ^4;                                 // give me 4th from last index
+WriteLine(words[idx]);
 
+Range rng = 3..^1;                              // 3rd to last
+wordRange = words[rng]; 
+WriteLine($"{string.Join(",", wordRange)}");
