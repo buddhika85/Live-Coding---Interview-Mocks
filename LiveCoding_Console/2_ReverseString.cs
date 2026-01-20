7d8bd408360bca5fc8cfeab4f137b03f5ce883c1
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2026-01-20 15:54:09 +1100

    3.Reverse a string.

---
+++ LiveCoding_Console/2_ReverseString.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace LiveCoding_Console
+{
+    internal class _2_ReverseString
+    {
+        public static void Test()
+        {
+            var str = "aBc123";
+            
+            Console.WriteLine($"{str} -> Reversed: {Reverse(str)}");
+        }
+
+        public static string Reverse(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return input;
+
+            char[] chars = input.ToCharArray();
+            int left = 0;
+            int right = chars.Length - 1;
+
+            while (left < right)
+            {
+                (chars[left], chars[right]) = (chars[right], chars[left]);
+                left++;
+                right--;
+            }
+
+            return new string(chars);
+        }
+
+    }
+}
--- LiveCoding_Console/Program.cs
+++ LiveCoding_Console/Program.cs
@@ -3,4 +3,6 @@
 Console.WriteLine("Hello - Live Coding Mock Interviews !");
 
 //new Mock12();
-_1_Deduplication.Test();
+//_1_Deduplication.Test();
+
+_2_ReverseString.Test();
